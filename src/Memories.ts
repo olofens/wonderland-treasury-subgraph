b86d53358e83853fe9ab9753a138a1a015844460
Olof <enstromolof@gmail.com>
2021-12-18 14:55:58 +0100

    Make work

--- src/Memories.ts
+++ src/Memories.ts
@@ -1,12 +1,12 @@
+import { LogRebase } from "./../generated/Memories/Memories";
 import { Address } from "@graphprotocol/graph-ts";
 import { MIM } from "../generated/MIM/MIM";
-import { RebaseCall } from "./../generated/WonderlandTimeStaking/WonderlandTimeStaking";
 import { MIM_CONTRACT, TREASURY_ADDRESS_1 } from "./utils/Constants";
 import { toDecimal } from "./utils/Decimals";
 import { loadOrCreateProtocolMetric } from "./utils/ProtocolMetrics";
 
-export function handleRebase(call: RebaseCall): void {
-  let timestamp = call.block.timestamp;
+export function handleLogRebase(event: LogRebase): void {
+  let timestamp = event.block.timestamp;
   let protocolMetric = loadOrCreateProtocolMetric(timestamp);
 
   let mimContract = MIM.bind(Address.fromString(MIM_CONTRACT));
